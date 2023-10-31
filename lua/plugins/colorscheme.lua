2f9754597a8dcf6e345a667bc606a5973580a26b
Redhawk18 <redhawk76767676@gmail.com>
2023-10-31 13:21:34 -0400

    basic lsp setup for python and rust

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return { 
+	-- "rebelot/kanagawa.nvim", -- https://github.com/rebelot/kanagawa.nvim
+	"ellisonleao/gruvbox.nvim", -- https://github.com/ellisonleao/gruvbox.nvim
+	priority = 1000,
+	config = function()
+		-- kanagawa
+		-- vim.cmd("colorscheme kanagawa-wave")
+
+		-- gruvbox
+		vim.o.background = "dark"
+		vim.cmd("colorscheme gruvbox")
+	end
+}
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+rm -rf ~/.config/nvim
+ln -s $(pwd) ~/.config/nvim
+

07f26406c6bac22780e31c595affb163487f1562
Redhawk18 <redhawk76767676@gmail.com>
2024-01-04 14:29:46 -0500

    comment out lsp's i never use and always cause problems

--- lua/options.lua
+++ lua/options.lua
@@ -1,8 +1,11 @@
 vim.o.background = "dark"
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 vim.opt.autoindent = true
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
-
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,56 +1,55 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_tool_installer = require("mason-tool-installer")
-		mason.setup({
-			ui = {
-				icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
-				},
-			},
-		})
+    "williamboman/mason.nvim",
+    dependencies = {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_tool_installer = require("mason-tool-installer")
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				-- bash
-				"bash-language-server",
-				"bash-debug-adapter",
-				"shfmt",
-				-- c, c++
-				"clangd",
-				-- css
-				"css-lsp",
-				-- docker
-				"dockerfile-language-server",
-				"docker-compose-language-service",
-				-- html
-				"html-lsp",
-				-- javascript, typescript
-				"typescript-language-server",
-				-- lua
-				"lua-language-server",
-				-- nix
-				"nil",
-				-- python
-				"pyright",
-				"pydocstyle",
-				"black",
-				-- ruby
-				"ruby-lsp",
-				-- rust
-				"rust-analyzer",
-				"codelldb",
-				-- sql
-				"sqlls",
-			},
-			auto_update = true,
-		})
-	end,
+        mason_tool_installer.setup({
+            ensure_installed = {
+                -- bash
+                "bash-language-server",
+                "bash-debug-adapter",
+                "shfmt",
+                -- c, c++
+                "clangd",
+                -- css
+                -- "css-lsp",
+                -- docker
+                -- "dockerfile-language-server",
+                -- "docker-compose-language-service",
+                -- html
+                "html-lsp",
+                -- javascript, typescript
+                -- "typescript-language-server",
+                -- lua
+                "lua-language-server",
+                -- nix
+                -- "nil",
+                -- python
+                "pyright",
+                "pydocstyle",
+                "black",
+                -- ruby
+                -- "ruby-lsp",
+                -- rust
+                "rust-analyzer",
+                "codelldb",
+                -- sql
+                -- "sqlls",
+            },
+            auto_update = true,
+        })
+    end,
 }
-

f6980ec0f36bfbbea47ed90d13bc281805f952f1
Redhawk18 <redhawk76767676@gmail.com>
2024-01-07 15:45:59 -0500

    added c and c++ to autoformat

--- lua/plugins/format-on-save.lua
+++ lua/plugins/format-on-save.lua
@@ -1,28 +1,30 @@
 return {
-	"elentok/format-on-save.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local format_on_save = require("format-on-save")
-		local formatters = require("format-on-save.formatters")
+    "elentok/format-on-save.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local format_on_save = require("format-on-save")
+        local formatters = require("format-on-save.formatters")
 
-		format_on_save.setup({
-			exclude_path_patterns = {
-				".venv/",
-				"build/",
-				"node_modules/",
-				"target/",
-			},
-			formatter_by_ft = {
-				css = formatters.lsp,
-				html = formatters.lsp,
-				javascript = formatters.lsp,
-				lua = formatters.lsp,
-				python = formatters.black,
-				rust = formatters.lsp,
-				sh = formatters.shfmt,
-				typescript = formatters.lsp,
-			},
-		})
-	end,
+        format_on_save.setup({
+            exclude_path_patterns = {
+                ".venv/",
+                "build/",
+                "node_modules/",
+                "target/",
+            },
+            formatter_by_ft = {
+                c = formatters.lsp,
+                cpp = formatters.lsp,
+                css = formatters.lsp,
+                html = formatters.lsp,
+                javascript = formatters.lsp,
+                lua = formatters.lsp,
+                python = formatters.black,
+                rust = formatters.lsp,
+                sh = formatters.shfmt,
+                typescript = formatters.lsp,
+            },
+        })
+    end,
 
 }

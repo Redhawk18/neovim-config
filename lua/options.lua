8db103e5e102f0c08441e9ef674558c02507aebb
Carson Range <crange76@gmail.com>
2024-02-15 13:46:35 -0500

    undotree

--- lua/options.lua
+++ lua/options.lua
@@ -1,11 +1,12 @@
 vim.o.background = "dark"
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.scrolloff = 10
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 
 vim.opt.autoindent = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4

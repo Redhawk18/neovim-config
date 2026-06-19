1fdc35748d5cdf53dd669902003c8f1e323c4f21
Redhawk18 <redhawk76767676@gmail.com>
2026-06-18 23:26:05 -0400

    rustfmt for rust

--- lsp/nixd.lua
+++ lsp/nixd.lua
@@ -1,3 +1,5 @@
 return {
-vim.lsp.config("nixd", { cmd = {"nixd"}, filetypes = {"nix"}, root_markers = {"flake.nix", "configuration.nix", ".git"},
-}), }
+    cmd = { "nixd" },
+    filetypes = { "nix" },
+    root_markers = { "flake.nix", "configuration.nix", ".git" },
+}

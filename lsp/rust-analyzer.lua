f00e60192f809ce86625067babe30081f742a9ec
Redhawk18 <redhawk76767676@gmail.com>
2026-06-19 00:22:48 -0400

    target dir caching ra

--- lsp/rust-analyzer.lua
+++ lsp/rust-analyzer.lua
@@ -1,5 +1,17 @@
 return {
-	cmd = { "rust-analyzer" },
-	root_markers = { ".git", "Cargo.toml", "cargo.toml" },
-	filetypes = { "rust" },
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    root_markers = { ".git", "Cargo.toml", "cargo.toml" },
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                targetDir = true,
+                allTargets = false,
+            },
+            check = {
+                command = "check",
+                workspace = false,
+            },
+        },
+    },
 }
